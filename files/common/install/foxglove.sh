bf4b3b116af8ee64ffc8ffec87412bbdc484fd94
Alex Fernandes <alex.fernandes@mov.ai>
2021-11-09 12:11:01 +0000

    fixes

--- files/common/install/foxglove.sh
+++ files/common/install/foxglove.sh
@@ -2,8 +2,12 @@
 ### every exit != 0 fails the script
 set -e
 set -u
-FOXGLOVE_VERSION="0.17.0"
+FOXGLOVE_VERSION="0.21.0"
 echo "Install Foxglove - a web based rviz alternative"
-wget https://github.com/foxglove/studio/releases/download/v0.17.0/foxglove-studio-$FOXGLOVE_VERSION-linux-amd64.deb
+wget https://github.com/foxglove/studio/releases/download/v${FOXGLOVE_VERSION}/foxglove-studio-${FOXGLOVE_VERSION}-linux-amd64.deb
 apt-get install -y ./foxglove-studio-$FOXGLOVE_VERSION-linux-amd64.deb
 rm -rf ./foxglove-studio-$FOXGLOVE_VERSION-linux-amd64.deb
+
+## update to newer version
+# apt update
+# apt install foxglove-studio
