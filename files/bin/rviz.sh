6a131dec3e17373ef127d6a5ab8af1a56de480b2
mithun <mithun@mov.ai>
2022-01-10 18:12:59 +0000

    Add ce docker file: lean version of noetic, 568MB after build, Update rviz.sh script

--- files/bin/rviz.sh
+++ files/bin/rviz.sh
@@ -2,6 +2,4 @@
 # File: rviz.sh
 set -e
 
-source /opt/ros/${ROS_DISTRO}/setup.bash
-
-ROS_MASTER_URI="http://${ROS_MASTER}:${ROS_MASTER_PORT}" /opt/ros/${ROS_DISTRO}/bin/rviz
+ROS_MASTER_URI="http://${ROS_MASTER}:${ROS_MASTER_PORT}" /usr/bin/rviz
