237429be1fbeeb984bea1eb5a8ac74682d61b3ae
AlexFernandes-MOVAI <alex.fernandes@mov.ai>
2026-07-07 12:07:00 +0100

    Update pre-commit hooks and enhance ROS environment setup in rviz2 and .bashrc

--- files/bin/rviz2.sh
+++ files/bin/rviz2.sh
@@ -8,5 +8,12 @@ else
     echo "ROS Foxy setup.bash not found"
 fi
 
+ROS2_DDS_DISCOVERY_SERVER="${ROS2_DDS_DISCOVERY_SERVER:-ros2-workspace}"
+ROS2_DDS_DISCOVERY_SERVER_PORT="${ROS2_DDS_DISCOVERY_SERVER_PORT:-11811}"
+
 export DISABLE_ROS1_EOL_WARNINGS=1
-ROS_MASTER_URI="http://${ROS_MASTER}:${ROS_MASTER_PORT}" "/opt/ros/foxy/bin/rviz2" "-d" "/headless/ros2-default.rviz"
+export ROS_MASTER_URI="http://${ROS_MASTER}:${ROS_MASTER_PORT}"
+export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+export ROS_DISCOVERY_SERVER="${ROS2_DDS_DISCOVERY_SERVER}:${ROS2_DDS_DISCOVERY_SERVER_PORT}"
+
+"/opt/ros/foxy/bin/rviz2" "-d" "/headless/ros2-default.rviz"
