64d5cc9a5862e92aba4398c5e732217866807207
AlexFernandes-MOVAI <alex.fernandes@mov.ai>
2026-08-24 12:28:27 +0100

    Refactor ROS setup in rqt.sh and update .bashrc for improved compatibility with ROS2

--- files/bin/rqt.sh
+++ files/bin/rqt.sh
@@ -11,5 +11,15 @@ else
     echo "ROS $ROS_DISTRO setup.bash not found"
 fi
 
-export DISABLE_ROS1_EOL_WARNINGS=1
+if [ "$ROS_DISTRO" != "noetic" ]; then
+    ROS2_DDS_DISCOVERY_SERVER="${ROS2_DDS_DISCOVERY_SERVER:-ros2-workspace}"
+    ROS2_DDS_DISCOVERY_SERVER_PORT="${ROS2_DDS_DISCOVERY_SERVER_PORT:-11811}"
+    export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+    export ROS_DISCOVERY_SERVER="${ROS2_DDS_DISCOVERY_SERVER}:${ROS2_DDS_DISCOVERY_SERVER_PORT}"
+    export FASTRTPS_DEFAULT_PROFILES_FILE=/headless/fastdds_udp_only.xml
+    export ROS_SUPER_CLIENT=TRUE
+else
+    export DISABLE_ROS1_EOL_WARNINGS=1
+    export ROS_MASTER_URI="http://${ROS_MASTER}:${ROS_MASTER_PORT}"
+fi
 "/opt/ros/$ROS_DISTRO/bin/rqt"
